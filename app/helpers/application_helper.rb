3b44e73ead15cf684a34433d0faa20990b97918f
honza-rychtar <honza.rychtar@gmail.com>
2015-09-27 22:47:31 +0200

    Dynamicke generovani page title. Lokalizace menu a paticky.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = t(:app_name)
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
 end
