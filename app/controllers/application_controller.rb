d6fe076395be2817ac43314ce221ed0527b464cd
honza-rychtar <honza.rychtar@gmail.com>
2015-09-27 23:41:27 +0200

    Prepinani jazyka v menu.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  before_action :set_locale
+ 
+	def set_locale
+		if params[:locale]
+			if params[:locale] == 'cs' || params[:locale] == 'en'
+	  		I18n.locale = params[:locale]
+	  	end
+	  end
+	end
 end
