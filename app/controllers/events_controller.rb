adc67704114d80dfc3ee7c635333e84a255c3ab5
honza-rychtar <honza.rychtar@gmail.com>
2015-10-18 04:29:02 +0200

    Uprava a mazani akci

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,27 +1,51 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
-    @events = Event.all
+    @events = Event.all.order(date: :asc)    
   end
 
-  def show
+  def show    
   end
+
+  def edit
+  end  
 
   def new
     @event = Event.new
+    @event.date = Date.today
   end
 
   def create
     @event = Event.new(event_params)
     respond_to do |format|
       if @event.save
-        format.html { redirect_to events_url, notice: 'Akce byla vytvořena' }
+        format.html { redirect_to events_url, notice: 'Akce byla vytvořena.' }
         format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new }
         format.json { render json: @event.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to events_url, notice: 'Akce byla upravena.' }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: 'Akce byla odstraněna.' }
+      format.json { head :no_content }
     end
   end
 
