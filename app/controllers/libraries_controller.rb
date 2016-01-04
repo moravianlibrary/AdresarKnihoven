4bb02c063e9eccb44583878db2c96c28fca3ce58
honza-rychtar <honza.rychtar@gmail.com>
2016-01-04 01:33:50 +0100

    Filtrovani podle kraju a sluzeb

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -10,8 +10,10 @@ class LibrariesController < ApplicationController
   # GET /libraries
   # GET /libraries.json
   def index
-    @show_results = params.has_key?(:q) || (params.has_key?(:lon) && params.has_key?(:lat))
+    @show_results = true # params.has_key?(:q) || (params.has_key?(:lon) && params.has_key?(:lat))
     @query = params[:q];
+    @region = params[:region];
+    @service_codes = nil
     q = @query.downcase unless @query.nil?        
     if params[:lon] && params[:lat]
       @all = Library.where("ABS(latitude - #{params[:lat]}) < 0.02 AND ABS(longitude - #{params[:lon]}) < 0.02")
@@ -20,16 +22,31 @@ class LibrariesController < ApplicationController
       if @query.nil?
         @all = Library.all
       else        
-        @all = Library.where("LOWER(name) #{like_clause} ? OR LOWER(sigla) = ? OR LOWER(code) = ? OR LOWER(city) #{like_clause} ?", "%#{q}%", "#{q.delete(' ')}", "#{q}", "#{q}%")
+        @all = Library.where("LOWER(libraries.name) #{like_clause} ? OR LOWER(sigla) = ? OR LOWER(libraries.code) = ? OR LOWER(city) #{like_clause} ?", "%#{q}%", "#{q.delete(' ')}", "#{q}", "#{q}%")
       end     
       @all = @all.where(active:true) if params[:inactive].nil?     
       if @all.count == 1 && request.format.html?
         redirect_to action: "show", id: @all[0].sigla
       end
     end
-    if @show_results
-      @libraries = @all.order(:priority, :name).paginate(page: params[:page], per_page: 20)   
+    if !@region.nil?
+      @all = @all.where(region: @region)
     end
+    @region_map = @all.group(:region).count(:region)    
+    @service_map = @all.joins(:services).group("services.code").count("services.code")
+    if !params[:services].blank?
+      @service_codes = params[:services].split(',')
+      @all = @all.joins(:services).where('services.code in (?)', @service_codes).group('libraries.id').having('COUNT(*) = ?', @service_codes.length)      
+      #@service_map = @all.select("services.*").group("services.code").count("services.code")
+    else
+      @service_codes = []      
+    end 
+    #@all = @all.select("libraries.*")
+    #@region_map = []
+    #@region_map = Library.find_by_sql('SELECT COUNT(region) AS count_region, region AS region FROM (SELECT "libraries".* FROM "libraries" INNER JOIN "libraries_services" ON "libraries_services"."library_id" = "libraries"."id" INNER JOIN "services" ON "services"."id" = "libraries_services"."service_id" WHERE (services.code in ("09")) GROUP BY libraries.id HAVING COUNT(*) = 1) GROUP BY region')
+      
+    @services = Service.all      
+    @libraries = @all.order(:priority, :name).paginate(page: params[:page], per_page: 20)       
   end
 
   # GET /libraries/1
--- app/models/library.rb
+++ app/models/library.rb
@@ -8,6 +8,7 @@ class Library < ActiveRecord::Base
 	has_one :opening_hour, dependent: :destroy
 	has_and_belongs_to_many :services
 	has_and_belongs_to_many :projects
+
 	def self.search(search, page)
 		order('name').where('LOWER(name) LIKE ? OR LOWER(sigla) = ? OR LOWER(code) = ? OR LOWER(city) = ?', "%#{search}%", "#{search}", "#{search}", "#{search}").order(:name).paginate(page: page, per_page: 20)
 	end
